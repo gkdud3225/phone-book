20a8d9fdf34a75a78357e90e4386365cc27fdfbe
hayeong <gkdud3225@hanmail.net>
2018-06-29 17:28:13 +0900

    전화번호 input 추가

--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react';
 
 class PhoneForm extends Component {
     state = {
-        name = ''
+        name: '',
+        phone: ''
     }
 
     handleChange = (e) => {
         this.setState({
-            name: e.target.value
-        })
+            [e.target.name]: e.target.value
+        });
     }
 
-    render(){
+    render() {
         return (
             <form>
-                <input placeholder="이름" value={this.state.name} onChange={this.handleChange} />
-                <div>{this.state.name}</div>
+                <input placeholder="이름" value={this.state.name} onChange={this.handleChange} name="name" />
+                <input placeholder="전화번호" value={this.state.phone} onChange={this.handleChange} name="phone" />
+                <div>{this.state.name} {this.state.phone}</div>
             </form>
         );
     }
